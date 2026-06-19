27734feca7bbb4e8ff7a9880ce876282548297b2
dylpb <dylanpabri@gmail.com>
2026-06-19 16:47:51 -0600

    Se crea el paquete selecciones
    En este paquete se crea un enum Grupo y una clase seleccion con sus atributos y metodos, falta corregir buscarJugador y programar Agregar y eliminar jugador

---
+++ src/selecciones/Grupo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package selecciones;
+
+/**
+ *
+ * @author Dylan
+ */
+public enum Grupo {
+       
+A, B, C, D, E, F, G, H, I, J, K, L
+    
+}
--- src/selecciones/Seleccion.java
+++ src/selecciones/Seleccion.java
@@ -4,11 +4,77 @@
  */
 package selecciones;
 
+import java.util.Arrays;
+import jugadores.Jugador;
+import personasentrenador.Entrenador;
+
 /**
  *
  * @author Dylan
  */
 public class Seleccion {
  
+    protected String nombreSele;
+    protected Grupo grupo;
+    protected Jugador jugadores[];
+    protected Entrenador entrenador;
+     
+
+    public String getNombreSele() {
+        return nombreSele;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public Jugador[] getJugador() {
+        return jugadores;
+    }
+
+    public Entrenador getEntrenador() {
+        return entrenador;
+    }
+
+    public void setEntrenador(Entrenador entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public Seleccion(String nombreSele, Grupo grupo, Jugador[] jugadores, Entrenador entrenador) {
+        this.nombreSele = nombreSele;
+        this.grupo = grupo;
+        this.jugadores = jugadores;
+        this.entrenador = entrenador;
+    }
+    
+    public int buscarJugador(Jugador[] jugadores) {
+        for (int i = 0; i < 22; i++) {
+            if (jugadores == null ? jugadores [i] == null : jugadores.equals(i)) {
+              
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+
+    public void agregarJugador() {
+         
+    }
+    
+    public void eliminarJugador() {
+        
+    }    
+
+    @Override
+    public String toString() {
+        return "Seleccion: " + nombreSele + 
+                "\nGrupo: " + grupo +
+                "\nEntrenador: " + entrenador +
+                "\nJugador: " + Arrays.toString(jugadores);
+    }
+    
+    
+    
     
 }
